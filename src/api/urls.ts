8ab29c9c24b1be1c60b5a31d219eaed8e8f45654
Omak <webmaster2511@yandex.ru>
2023-12-03 20:44:46 +0700

    8.13. Оптимизируй это (часть 2)

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -2,7 +2,7 @@ export enum ApiUrl {
   BASE = 'https://14.react.pages.academy/six-cities',
   GET_OFFERS = `${ApiUrl.BASE}/offers`,
   GET_COMMENTS = `${ApiUrl.BASE}/comments`,
+  FAVORITES = `${ApiUrl.BASE}/favorite`,
   LOGIN = `${ApiUrl.BASE}/login`,
   LOGOUT = `${ApiUrl.BASE}/logout`,
-  FAVORITES = `${ApiUrl.BASE}/favorite`
 }
